597830abe0b8015c2abf3067956a297f9c58d954
Cody Boggs <cboggs@rallydev.com>
2014-08-28 14:52:05 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This repo is simply a dumping ground for some interesting Grafana scripted dashboard ideas... for now.
+
+At present, I've only included a simple "wrapper" dashboard script that will source in a "template" script based on the `template` URL argument.
+
+To use these in your Grafana install (>= 1.6.1), just add `cboggs_demo.js` to <grafana_home>/app/dashboards/, and `jdk_8.js` to <grafana_home>/.
+
+Appropriate arguments are commented in the top of `cboggs_demo.js`.
+
+More to come!
---
+++ cboggs_demo.js
@@ -0,0 +1,106 @@
+/* global _ */
+
+/*
+URL args:
+service           - Currently only used for setting the title of the dashboard
+prefix            - (optional) InfluxDB series name prefix, if you use them
+hostname          - Regex for hosts you want to graph. This uses the InfluxDB query regex syntax in the WHERE clause
+template          - What graphs do you want to see? Template scripts should return a single function that is named 
+                    the same as the template file itself, sans '.js'. Said function should return a valid array of
+                    Grafana panels. This function currently needs to accept a hostname (regex), prefix, and aggregate
+                    boolean. Template scripts can live most anywhere inside $GRAFANA_HOME except /app/dashboards/.  
+agg [true|false]  - True will attempt to show aggregate values across all regex-matched hosts, false will generate a
+                    row per hostname containing the appropriate panels
+from              - Start of time window
+*/
+
+'use strict';
+
+// accessable variables in this scope
+var window, document, ARGS, $, jQuery, moment, kbn;
+
+function find_hostnames(query) {
+    var search_url = window.location.protocol + '//bld-influxdb-01/db/testdb/series?u=testuser&p=testpw&q=' + query;
+    var results = [];
+    var request = new XMLHttpRequest();
+    request.open('GET', search_url, false);
+    request.send(null);
+    var obj = JSON.parse(request.responseText);
+
+    for (var i = 0; i < obj[0]["points"].length; i++) {
+        results.push(obj[0]["points"][i][1]);
+    };
+
+    return results.sort();
+};
+
+return function(callback) {
+
+    // Setup some variables
+    var dashboard, timspan, panels;
+
+    var rowHeight = '200px';
+    var argHostname = (ARGS.host || 'bogus.foo.doman');
+    var argPrefix = (ARGS.prefix || '');
+    var argService = (ARGS.service || 'Bogus');
+    var argTemplate = (ARGS.template || 'foo_template.js');
+    var argAggregate;
+
+    if (!_.isUndefined(ARGS.agg) && ARGS.agg === "true") {
+        argAggregate = true;
+    } else {
+        argAggregate = false;
+    }
+
+    // Set a default timespan if one isn't specified
+    timspan = '1d';
+
+    // Intialize a skeleton with nothing but a rows array and service object
+    dashboard = {
+        rows: [],
+        services: {}
+    };
+
+    // Set a title
+    dashboard.title = argAggregate ? argService + ' Aggregate' : argService;
+
+    dashboard.services.filter = {
+        time: {
+            from: "now-" + (ARGS.from || timspan),
+            to: "now"
+        }
+    };
+
+    $("head").append('<script type="text/javascript" src="/' + argTemplate + '.js"></script>');
+
+    $.ajax({
+        method: 'GET',
+        url: '/'
+    })
+        .done(function(result) {
+            if (argAggregate) {
+                dashboard.rows.push({
+                    title: argHostname.split(".")[0],
+                    height: rowHeight,
+                    panels: window[argTemplate](argHostname, argPrefix, argAggregate),
+                });
+            } else {
+                var hosts = find_hostnames('select distinct(hostname) from allHosts where hostname =~ /' + argHostname + '/');
+                console.log(hosts.length);
+
+                for (var i = 0; i < hosts.length; i++) {
+                    dashboard.rows.push({
+                        title: 'Chart',
+                        height: rowHeight,
+                        panels: window[argTemplate](hosts[i], argPrefix, argAggregate, i + 1),
+                    });
+                }
+            }
+
+            // when dashboard is composed call the callback
+            // function and pass the dashboard
+            // console.log(dashboard);
+            callback(dashboard);
+
+        });
+}
